e4e89006422930a07408cd5584de7342c5611275
Naveen <navinkumar618@gmail.com>
2022-01-16 23:21:01 +0530

    Socks program

---
+++ Warm-up/socks.js
@@ -0,0 +1,38 @@
+//
+
+let size = 5;
+let arr = [1, 2, 1, 2, 3];
+
+console.log(`Size: ${size}`);
+console.table(arr);
+
+function pairOfSocks(size, ar) {
+  // Vaccination
+  if (!ar || ar.length !== size) return 0;
+
+  let numberOfPairs = 0;
+  let newSocksCount = new Map();
+
+
+  // check for pairs from the array
+  for (let i = 0; i < ar.length; i++) {
+    let sock = ar[i];
+
+    if (!newSocksCount.has(sock)) {
+      newSocksCount.set(sock, 1);
+    } else {
+      let count = newSocksCount.get(sock) + 1;
+      newSocksCount.set(sock, count);
+    }
+  }
+
+  // get pair of socks
+  for (let type of newSocksCount.keys()) {
+    let pair = newSocksCount.get(type);
+    numberOfPairs += Math.floor(pair / 2);
+  }
+  console.table(newSocksCount);
+  return numberOfPairs;
+}
+
+console.log("Total Number of Pairs: ", pairOfSocks(size, arr));
